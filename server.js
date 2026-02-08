231d08e91e22ebede51e107427d8171810b49f35
Ibrahim Solkar <ibrahimsolkar0@gmail.com>
2026-02-08 14:08:07 +0530

    Configure for Vercel deployment

--- server.js
+++ server.js
@@ -30,8 +30,10 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+}
 
 module.exports = app;
